89d54daf24a25b2c9af2b79d528592b0d20a76ad
Yuki Sobue <yuki.sobue@gmail.com>
2019-06-22 12:39:24 +0900

    データベースにカラムを追加
    user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_21_041259) do
+ActiveRecord::Schema.define(version: 2019_06_22_014338) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
 end
